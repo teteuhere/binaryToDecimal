9193cab4d4931e198f765a186f72edacecdfd4d0
Matheus S <64415336+teteuhere@users.noreply.github.com>
2023-12-21 19:11:28 -0300

    Update binaryToDecimal.java

--- src/binaryToDecimal.java
+++ src/binaryToDecimal.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-
 public class binaryToDecimal {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter a binary number (up to 8 digits): ");
         String binaryInput = scanner.nextLine().trim();
-
         if (isValidBinary(binaryInput)) {
             int decimalResult = binaryToDecimal(binaryInput);
             System.out.println("Decimal equivalent: " + decimalResult);
         } else {
             System.out.println("Invalid binary input. Please enter only 0s and 1s.");
         }
-
         scanner.close();
     }
 
@@ -25,7 +21,6 @@ public class binaryToDecimal {
     private static int binaryToDecimal(String binaryInput) {
         int decimalResult = 0;
         int power = 0;
-
         for (int i = binaryInput.length() - 1; i >= 0; i--) {
             int digit = Character.getNumericValue(binaryInput.charAt(i));
             decimalResult += digit * Math.pow(2, power);
